0e5c84d6570eb9fd358305d5775e6a8395bc2ba4
prasadgrid5281 <vkanugula@griddynamics.com>
2024-12-25 01:05:14 +0530

    error handling to the api

--- src/utils/useSearchVideo.js
+++ src/utils/useSearchVideo.js
@@ -5,17 +5,26 @@ import { addSearchRelatedVideo } from "./searchSlice";
 const useSearchVideo = () => {
   const searchTerm = useSelector((store) => store.search.searchTermVideo);
   const dispatch = useDispatch();
-  const getSearchVideo = async () => {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&key=AIzaSyAbGC3KfbOVhJ7a5Fym9V0vtqYS9-4kJME`
-    );
-    const data = await response.json();
 
-    dispatch(addSearchRelatedVideo(data.items));
+  const getSearchVideo = async () => {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&key=AIzaSyAbGC3KfbOVhJ7a5Fym9V0vtqYS9-4kJME`
+      );
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      dispatch(addSearchRelatedVideo(data.items));
+    } catch (error) {
+      console.error("Failed to fetch search results:", error);
+    }
   };
+
   useEffect(() => {
     if (!searchTerm) return;
     getSearchVideo();
   }, [searchTerm]);
 };
+
 export default useSearchVideo;
