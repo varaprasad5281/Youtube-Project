71940b164a2b1e04cd90cd99caad75d232771504
prasadgrid5281 <vkanugula@griddynamics.com>
2024-12-26 19:21:53 +0530

    infinte scrolling added

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addUserName } from "../utils/userSlice";
 import { isAvatarChange } from "../utils/appSlice";
@@ -9,10 +9,22 @@ const Popup = () => {
   const dispatch = useDispatch();
   const isPopup = useSelector((store) => store.app.isAvatar);
 
+  const storedUserName = sessionStorage.getItem("userName");
+
+  useEffect(() => {
+    if (storedUserName) {
+      dispatch(addUserName(storedUserName));
+      dispatch(isAvatarChange());
+    }
+  }, [storedUserName, dispatch]);
+
   const handleUserInput = (e) => {
     e.preventDefault();
-    if (userName.current.value.trim()) {
-      dispatch(addUserName(userName.current.value.trim()));
+    const inputValue = userName.current.value.trim();
+
+    if (inputValue) {
+      sessionStorage.setItem("userName", inputValue);
+      dispatch(addUserName(inputValue));
       dispatch(isAvatarChange());
       setError(null);
     } else {
@@ -20,7 +32,8 @@ const Popup = () => {
     }
   };
 
-  if (!isPopup) return null;
+  // Do not show the popup if the username already exists in sessionStorage
+  if (!isPopup || storedUserName) return null;
 
   return (
     <div className="fixed top-[50%] left-[50%] max-w-96 w-full transform translate-x-[-50%] translate-y-[-50%] p-4 bg-white shadow-lg rounded-lg z-50">
@@ -32,7 +45,7 @@ const Popup = () => {
           type="text"
           placeholder="Enter your Name..."
         />
-        {error && <p className="text-red-500 text-sm">{error}</p>}{" "}
+        {error && <p className="text-red-500 text-sm">{error}</p>}
         <button
           type="submit"
           className="px-4 py-2 w-full bg-green-500 text-white rounded-lg"
